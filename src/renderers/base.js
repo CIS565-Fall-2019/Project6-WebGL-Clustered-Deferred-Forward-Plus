c9957931a4540441cb55d9301fcde05b66afcce5
Peyman Norouzi <41491728+pnorouzi@users.noreply.github.com>
2019-11-04 00:12:52 -0500

    started working on it

--- src/renderers/base.js
+++ src/renderers/base.js
@@ -1,4 +1,5 @@
 import TextureBuffer from './textureBuffer';
+import {mat4, vec4} from 'gl-matrix';
 
 export const MAX_LIGHTS_PER_CLUSTER = 100;
 
@@ -10,7 +11,7 @@ export default class BaseRenderer {
     this._ySlices = ySlices;
     this._zSlices = zSlices;
   }
-
+  
   updateClusters(camera, viewMatrix, scene) {
     // TODO: Update the cluster texture with the count and indices of the lights in each cluster
     // This will take some time. The math is nontrivial...
@@ -23,6 +24,68 @@ export default class BaseRenderer {
           this._clusterTexture.buffer[this._clusterTexture.bufferIndex(i, 0)] = 0;
         }
       }
+    }
+
+    var screenH = Math.tan((camera.fov/2.0) * (Math.PI / 180.0)) * 2.0;
+    var screenW = camera.aspect * screenH;
+    var screenD = camera.far - camera.near;
+
+    var xStride = screenH / this._xSlices;
+    var yStride = screenW / this._ySlices;
+    var zStride = screenD / this._zSlices;
+
+    for (let L_i=0;L_i<MAX_LIGHTS_PER_CLUSTER;L_i++){
+
+      var pos = scene.lights[L_i].position;
+      var lightPos = vec4.fromValues(pos[0],pos[1],pos[2],1.0);
+      radius = scene.lights[L_i].radius;
+
+      vec4.transformMat4(lightPos, lightPos, viewMatrix);
+      lightPos[2] *= -1.0;
+
+      var z_min = (lightPos[2] - radius) / zStride;
+      var z_max = (lightPos[2] + radius) / zStride;
+
+      z_min = Math.min(Math.max(Math.floor(z_min), 0), this._zSlices-1);
+      z_max = Math.min(Math.max(Math.floor(z_max), 0), this._zSlices-1);
+
+      xStride *= lightPos[2];
+
+      var x_min = ((lightPos[2]*(screenW/2.0)) + lightPos[0] - radius)/xStride;
+      var x_max = ((lightPos[2]*(screenW/2.0)) + lightPos[0] + radius)/xStride;
+      x_min = Math.min(Math.max(Math.floor(x_min)-1, 0), this._xSlices-1);
+      x_max = Math.min(Math.max(Math.floor(x_max)+1, 0), this._xSlices-1);
+
+
+      YStride *= lightPos[2];
+
+      var y_min = ((lightPos[2]*(screenH/2.0)) + lightPos[1] - radius)/yStride;
+      var y_max = ((lightPos[2]*(screenH/2.0)) + lightPos[1] + radius)/yStride;
+      y_min = Math.min(Math.max(Math.floor(y_min)-1, 0), this._ySlices-1);
+      y_max = Math.min(Math.max(Math.floor(y_max)+1, 0), this._ySlices-1);
+
+      for (let i2 = x_min;x_min<=x_max;i2++){
+        for(let j = y_min;y_min<=y_max;j2++){
+          for(let k = z_min;z_min<=z_max;k2++){
+
+            let index = (k * this._xSlices * this._ySlices) + (j * this._xSlices) + i2;
+            let numLightIdx = this._clusterTexture.bufferIndex(index, 0);
+            let numLight = this._clusterTexture.buffer[numLightIdx] + 1.0;
+
+            if (numLight <= MAX_LIGHTS_PER_CLUSTER){
+
+              this._clusterTexture.buffer[numLightIdx] = numLight;
+
+              let numLightIdx2 = this._clusterTexture.bufferIndex(index, Math.floor(numLight/4.0));
+              
+              this._clusterTexture.buffer[numLightIdx2 + (numLight%4)] = i;
+              
+
+            }
+          }
+        }
+      }
+
     }
 
     this._clusterTexture.update();
